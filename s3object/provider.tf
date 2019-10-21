0e36513f91b286386d66656528ada966226ebd33
Yomi Ogunyinka <Yomi.O@JaghoConsultants.co.uk>
2019-10-21 08:28:53 +0100

    Delete provider.tf

--- s3object/provider.tf
+++
@@ -1,9 +0,0 @@
-# Setup our AWS Provider..!
-
-provider "aws" {
-  # version = "~> 2.22.0"
-
-  shared_credentials_file = "/../../.aws/credentials"
-  profile                 = "dev-jcs"
-  region                  = "${var.region}"
-}
